f59c0455d3c4be0cce50a5239a7141f0060622a1
Benedict Anthony <benedict.ughulu@gmail.com>
2022-08-06 17:41:30 -0700

    Firts commit

--- yummy/settings.py
+++ yummy/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
+import django_heroku
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -23,9 +24,9 @@ import os
 SECRET_KEY = 'django-insecure-o742(+b(r(xjg@=)!wn_2ug-v5)q-yntzw%v1na=z6j-u3cxql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yummykicthen.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -88,10 +89,20 @@ WSGI_APPLICATION = 'yummy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':config("DATABASE"),
+        'USER': config("USER"),
+        'HOST':  config("HOST"),
+        'PASSWORD': config("PASSWORD"),
+        'PORT': config("PORT"),
     }
 }
 
@@ -205,7 +216,9 @@ CLOUDINARY_STORAGE = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+django_heroku.settings(locals())
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 MEDIA_URL = "/media/"
